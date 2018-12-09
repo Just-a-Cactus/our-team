9b6edd5e1a8322c132ddc1f877ae970dd72b4c34
frankovich <prostofrankovich@gmail.com>
2018-12-09 13:48:50 +0200

    fix slider for AlexCSS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 (function ($) {
     const slider = $('.ba-team-slider');
     slider.slick({
+        dots: true,
+        dotsClass: 'ssd',
         prevArrow: '.ba-button--prev',
         nextArrow: '.ba-button--next'
     });
